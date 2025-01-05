44846d43d85d34ad16704923dae6a1d5ff7d078c
jakecoombs <57802613+jakecoombs@users.noreply.github.com>
2025-01-05 20:56:09 +0000

    Add themes

--- app/(tabs)/about.tsx
+++ app/(tabs)/about.tsx
@@ -1,21 +1,19 @@
+import { ThemedText } from "@/components/ThemedText";
+import { ThemedView } from "@/components/ThemedView";
 import { Text, View, StyleSheet } from "react-native";
 
 export default function AboutScreen() {
   return (
-    <View style={styles.container}>
-      <Text style={styles.text}>About screen</Text>
-    </View>
+    <ThemedView style={styles.container}>
+      <ThemedText>About screen</ThemedText>
+    </ThemedView>
   );
 }
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: "#25292e",
     justifyContent: "center",
     alignItems: "center",
-  },
-  text: {
-    color: "#fff",
   },
 });
