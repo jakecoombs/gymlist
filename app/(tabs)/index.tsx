e988e39f03d4b10f33013845c5a9f998eb85e8e1
jakecoombs <57802613+jakecoombs@users.noreply.github.com>
2025-01-05 21:23:23 +0000

    Style workout list

--- app/(tabs)/index.tsx
+++ app/(tabs)/index.tsx
@@ -1,12 +1,10 @@
-import { Text, View, StyleSheet } from "react-native";
+import { StyleSheet } from "react-native";
 import WorkoutList from "@/components/WorkoutList";
 import { ThemedView } from "@/components/ThemedView";
-import { ThemedText } from "@/components/ThemedText";
 
 export default function Index() {
   return (
     <ThemedView style={styles.container}>
-      <ThemedText>Workouts</ThemedText>
       <WorkoutList />
     </ThemedView>
   );
@@ -16,10 +14,5 @@ const styles = StyleSheet.create({
   container: {
     flex: 1,
     alignItems: "center",
-  },
-  button: {
-    fontSize: 20,
-    textDecorationLine: "underline",
-    color: "#fff",
   },
 });
--- components/WorkoutList.tsx
+++ components/WorkoutList.tsx
@@ -1,25 +1,46 @@
 import React from "react";
-import { FlatList, Text, View } from "react-native";
+import { FlatList, useColorScheme } from "react-native";
+import { StyleSheet } from "react-native";
 import { WORKOUTS } from "@/data";
 import { ThemedText } from "./ThemedText";
 import { ThemedView } from "./ThemedView";
+import { Colors } from "@/constants/Colors";
 
 const WorkoutList = () => {
   const workouts = WORKOUTS;
+  const colorScheme = useColorScheme() ?? "light";
 
   return (
     <ThemedView>
       <FlatList
         data={workouts}
         renderItem={({ item }) => (
-          <ThemedView>
-            <ThemedText>{item.name}</ThemedText>
-            <ThemedText>{item.description}</ThemedText>
+          <ThemedView
+            style={[
+              styles.container,
+              { borderColor: Colors[colorScheme].text },
+            ]}
+          >
+            <ThemedText type="defaultSemiBold">{item.name}</ThemedText>
+            <ThemedText type="subtext">{item.description}</ThemedText>
+            <ThemedText type="subtext">
+              Last workout: {item.lastWorkout?.split("T")[0] ?? "N/A"}
+            </ThemedText>
           </ThemedView>
         )}
+        keyExtractor={(item) => item.id.toString()}
       />
     </ThemedView>
   );
 };
+
+const styles = StyleSheet.create({
+  container: {
+    padding: 5,
+    marginVertical: 5,
+    borderRadius: 12,
+    borderWidth: 2,
+  },
+});
 
 export default WorkoutList;
